0c9609073617bb17a259a74af34d1e7bd5868b30
forsaken <alexey2142@mail.ru>
2016-05-06 23:04:56 +1000

    minify and delete some static files

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,21 +1,14 @@
 jQuery(document).ready(function($) {
-
-    /*======= Skillset *=======*/
-    
     $('.level-bar-inner').css('width', '0');
     
     $(window).on('load', function() {
-
         $('.level-bar-inner').each(function() {
-        
             var itemWidth = $(this).data('level');
             
             $(this).animate({
                 width: itemWidth
             }, 800);
-            
         });
-
     });
     
     /* Bootstrap Tooltip for Skillset */
@@ -23,7 +16,4 @@ jQuery(document).ready(function($) {
     
     /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
     GitHubCalendar("#github-graph", "forsaken1");
-    
-    /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
-    GitHubActivity.feed({ username: "forsaken1", selector: "#ghfeed" });
 });

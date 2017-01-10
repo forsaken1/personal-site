634b2ffffff86b9cddef072e5a341f17c8aa6161
forsaken <alexey2142@mail.ru>
2017-01-10 18:09:49 +1000

    add experience

--- app.rb
+++ app.rb
@@ -1,6 +1,19 @@
 require 'sinatra'
 require 'slim'
 require 'yaml'
+require 'byebug'
+
+def calculate_experience jobs
+  months, years = 0, 0
+  jobs.each do |job|
+    date_stop = job['date_stop'] == 'Present' ? Date.today : Date.parse(job['date_stop'])
+    months += date_stop.month - Date.parse(job['date_start']).month
+    years += date_stop.year - Date.parse(job['date_start']).year
+  end
+  years += months / 12
+  months = months % 12
+  yield years, months
+end
 
 get '/' do
   @content = YAML.load(ERB.new(File.read('config/content.yml')).result)
